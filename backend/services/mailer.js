b9d17c5a4d44c488b2d380b44d91bb2cd584ab1d
Vaibhav Chavhan <chavhanvaibhav708@gmail.com>
2026-04-23 09:20:16 +0000

    Add nodemailer mail service

---
+++ backend/services/mailer.js
@@ -0,0 +1,23 @@
+import nodemailer from "nodemailer";
+import config from "../config/config.js";
+
+const { emailUser, emailPass } = config;
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: emailUser,
+    pass: emailPass,
+  },
+  family: 6, // Force IPv6. If mail does not work on some networks, change this to 4.
+});
+
+export const sendMail = async ({ to, subject, html, replyTo, isAdmin }) => {
+  await transporter.sendMail({
+    from: `Disha Study Library <${emailUser}>`,
+    ...(isAdmin ? { bcc: to } : { to }),
+    replyTo,
+    subject,
+    html,
+  });
+};
