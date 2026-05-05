88698bf49452e2a9462e01c9dd2fbcdbf6685b10
Vaibhav Chavhan <chavhanvaibhav708@gmail.com>
2026-05-05 08:40:01 +0000

    Add resend mailer service

--- backend/services/mailer.js
+++ backend/services/mailer.js
@@ -1,22 +1,10 @@
-import nodemailer from "nodemailer";
-import config from "../config/config.js";
+import { Resend } from "resend";
 
-const { emailUser, emailPass } = config;
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: emailUser,
-    pass: emailPass,
-  },
-  family: 4,
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendMail = async ({ to, subject, html, replyTo, isAdmin }) => {
-  await transporter.sendMail({
-    from: `Disha Study Library <${emailUser}>`,
+  await resend.emails.send({
+    from: `Disha Study Library <onboarding@resend.dev>`,
     ...(isAdmin ? { bcc: to } : { to }),
     replyTo,
     subject,
