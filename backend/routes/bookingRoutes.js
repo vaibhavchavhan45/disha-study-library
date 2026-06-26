b11454e547c67f49f6188d6aa4109e34db42d399
Vaibhav Chavhan <chavhanvaibhav708@gmail.com>
2026-06-26 06:27:46 +0000

    fix:auth middleware to GET /booking/all

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { submitBooking, getBookings } from "../controllers/booking/bookingController.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/submit", submitBooking);
-router.get("/all", getBookings);
+router.get("/all", authMiddleware, getBookings);
 
 export default router;
